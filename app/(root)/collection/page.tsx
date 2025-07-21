ac4e9b21d6575fdb6487386d903bf6083c61d9fc
Fabio Carucci <garu2000@live.it>
2025-07-21 22:20:23 +0200

    Integrate Filters Everywhere

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,61 +1,55 @@
-import Link from "next/link";
-
 import QuestionCard from "@/components/cards/QuestionCard";
 import DataRenderer from "@/components/DataRenderer";
-import HomeFilter from "@/components/filters/HomeFilter";
+import CommonFilter from "@/components/filters/CommonFilter";
 import LocalSearch from "@/components/search/LocalSearch";
-import { Button } from "@/components/ui/button";
+import { CollectionFilters } from "@/constants/filters";
 import ROUTES from "@/constants/routes";
 import { EMPTY_QUESTION } from "@/constants/states";
-import { getQuestions } from "@/lib/actions/question.action";
+import { getSavedQuestions } from "@/lib/actions/collection.action";
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
-const Home = async ({ searchParams }: SearchParams) => {
+const Collections = async ({ searchParams }: SearchParams) => {
   const { page, pageSize, query, filter } = await searchParams;
 
-  const { success, data, error } = await getQuestions({
+  const { success, data, error } = await getSavedQuestions({
     page: Number(page) || 1,
     pageSize: Number(pageSize) || 10,
     query: query || "",
     filter: filter || "",
   });
 
-  const { questions } = data || {};
+  const { collection } = data || {};
 
   return (
     <>
-      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
 
-        <Button
-          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
-          asChild
-        >
-          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
-        </Button>
-      </section>
-      <section className="mt-11">
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearch
-          route="/"
+          route={ROUTES.COLLECTION}
           imgSrc="/icons/search.svg"
           placeholder="Search questions..."
           otherClasses="flex-1"
         />
-      </section>
-      <HomeFilter />
+
+        <CommonFilter
+          filters={CollectionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
 
       <DataRenderer
         success={success}
         error={error}
-        data={questions}
+        data={collection}
         empty={EMPTY_QUESTION}
-        render={(questions) => (
+        render={(collection) => (
           <div className="mt-10 flex w-full flex-col gap-6">
-            {questions.map((question) => (
-              <QuestionCard key={question._id} question={question} />
+            {collection.map((item) => (
+              <QuestionCard key={item._id} question={item.question} />
             ))}
           </div>
         )}
@@ -64,4 +58,4 @@ const Home = async ({ searchParams }: SearchParams) => {
   );
 };
 
-export default Home;
+export default Collections;
